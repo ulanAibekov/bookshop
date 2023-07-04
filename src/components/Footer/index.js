327a9138672f065c4046eeda651def22e4b5e500
aichurokdev <aichurokkuttubekovna1@gmail.com>
2023-07-04 11:47:19 +0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,49 +1,58 @@
-import React from 'react';
-import "./style.scss"
-import {BsFacebook, BsInstagram, BsLinkedin, BsTelegram, BsTelephoneOutboundFill, BsWhatsapp} from "react-icons/bs";
-import {TfiEmail} from "react-icons/tfi";
-import {HiLocationMarker} from "react-icons/hi";
+import React from "react";
+import './style.scss';
+import {BsFacebook, BsInstagram, BsTelegram, BsWhatsapp} from "react-icons/bs";
 
 const Footer = () => {
     return (
-        <div id="footer">
+        <div id = 'footer'>
             <div className="container">
                 <div className="footer">
-                    <div className="footer--sing">
-                        <h1>Sign up for our newsletter</h1>
-                        <p>Be the first to know about our special offers, news, and updates.</p>
-                        <input type="text" placeholder="Email Address"/>
-                        <button>Sign Up</button>
-                    </div>
-
-                    <div className="footer--sing__lorem">
-                        <div className="footer--sing__lorem--sum">
-                            <h1>Contact:</h1>
-                            <a href="#"><BsTelephoneOutboundFill/> +7 (999) 99 99 99</a>
-                            <a className="footer--sing__lorem--sum__ic" href="#"><TfiEmail/> motionweb@gmail.com</a>
-                            <a href="tel:+996"><BsWhatsapp/> 0700 232 400</a>
-
+                    <div className="footer--block">
+                        <div className="footer--block__sing">
+                            <h1>Sign up for our newsletter</h1>
+                            <p>Be the first to know about our special offers, news, and updates.</p>
+                            <input type="text" placeholder="Email Address"/>
+                            <button>Sign Up</button>
                         </div>
-                        <div className="footer--sing__lorem--sum">
-                            <h1>Address:</h1>
-                            <h2>Kyrgystan/Bishkek</h2>
-                            <a href="#"><HiLocationMarker/> Turusbekova 109/3</a>
-                            <div className="footer--sing__lorem--sum__icons">
-                                <a href=""><BsLinkedin/></a>
-                                <a href="https://www.instagram.com/motion_web/"><BsInstagram/></a>
-                                <a href=""><BsFacebook/></a>
-                                <a href=""><BsTelegram/></a>
-
-
-                            </div>
+                        <div className="footer--block__read">
+                            <h2>Read</h2>
+                            <p>Book news</p>
+                            <p>Ratings BookShop</p>
+                            <p>Celebrity ratings</p>
+                            <p>Bestsellers</p>
+                        </div>
+                        <div className="footer--block__create">
+                            <h2>Create</h2>
+                            <p>Book collections</p>
+                            <p>Personal book profile</p>
+                        </div>
+                        <div className="footer--block__bookshop">
+                            <h2>BookShop</h2>
+                            <p>About us</p>
+                            <p>Contacts</p>
+                            <p>Site rules</p>
+                            <p>Privacy agreement</p>
                         </div>
                     </div>
-
-
-
+                    <div className="devider"></div>
+                    <div className="footer--icons">
+                        <div className="footer--icons__ai">
+                            < BsFacebook className = "bs"/>
+                        </div>
+                        <div className="footer--icons__ai">
+                            <BsInstagram  className = "bs"/>
+                        </div>
+                        <div className="footer--icons__ai">
+                            <BsTelegram  className = "bs"/>
+                        </div>
+                        <div className="footer--icons__ai">
+                            <BsWhatsapp  className = "bs"/>
+                        </div>
+                    </div>
+                    <div className="devider"></div>
+                    <p className='title'>© 2023 BookShop</p>
                 </div>
             </div>
-
         </div>
     );
 };
